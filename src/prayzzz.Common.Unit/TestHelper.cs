ded7ab8773387a784d003b1d0073ad38b0756b18
prayzzz <prayzzz@outlook.com>
2017-09-03 10:33:44 +0200

    - Options

--- src/prayzzz.Common.Unit/TestHelper.cs
+++ src/prayzzz.Common.Unit/TestHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Reflection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 
 namespace prayzzz.Common.Unit
@@ -26,6 +28,26 @@ namespace prayzzz.Common.Unit
             loggerFactory.Setup(x => x.CreateLogger(typeof(T).FullName)).Returns(new ConsoleLogger());
 
             return loggerFactory;
+        }
+
+        public static string ReadEmbeddedFile(Assembly assembly, string filePath)
+        {
+            var (result, content) = new ResourceLoader(new ConsoleLogger<ResourceLoader>()).ReadEmbeddedFile(assembly, filePath);
+
+            if (result.IsError)
+            {
+                throw result.Exception;
+            }
+
+            return content;
+        }
+
+        public static IOptions<T> CreateOptions<T>(T value) where T : class, new()
+        {
+            var optionsMock = Mock<IOptions<T>>();
+            optionsMock.Setup(x => x.Value).Returns(value);
+
+            return optionsMock.Object;
         }
     }
 }
