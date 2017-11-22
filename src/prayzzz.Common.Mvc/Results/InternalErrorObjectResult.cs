4b28539b0566e63b04242098a1624557fc05c344
prayzzz <prayzzz@outlook.com>
2017-11-22 15:06:37 +0100

    - add object results

---
+++ src/prayzzz.Common.Mvc/Results/InternalErrorObjectResult.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prayzzz.Common.Mvc.Results
+{
+    public class InternalErrorObjectResult : ObjectResult
+    {
+        public InternalErrorObjectResult(object value) : base(value)
+        {
+            StatusCode = 500;
+        }
+    }
+}
---
+++ src/prayzzz.Common.Mvc/Results/UnauthorizedObjectResult.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prayzzz.Common.Mvc.Results
+{
+    public class UnauthorizedObjectResult : ObjectResult
+    {
+        public UnauthorizedObjectResult(object value) : base(value)
+        {
+            StatusCode = 401;
+        }
+    }
+}
